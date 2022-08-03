cc1ee7222e97623ca86fa0e5fc2aee3eaf89c914
ruanshutian <1204970556@qq.com>
2022-08-03 18:07:40 +0800

    2_10

--- 02_7类型转换/script1.js
+++ 02_7类型转换/script1.js
@@ -20,8 +20,8 @@
 // alert( Boolean(1) ); // true
 // alert( Boolean(0) ); // false
 
-alert( Boolean("hello") ); // true
-alert( Boolean("") ); // false
+// alert( Boolean("hello") ); // true
+// alert( Boolean("") ); // false
 alert( Boolean("0") ); // true
 alert( Boolean("") );//false
 alert( Boolean(" ") );
