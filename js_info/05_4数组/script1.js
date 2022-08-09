faf29897a42fc634ce10a88e83963cf222f77e68
ruanshutian <1204970556@qq.com>
2022-08-09 14:54:37 +0800

    js

--- js_info/05_4数组/script1.js
+++ js_info/05_4数组/script1.js
@@ -35,15 +35,68 @@
 // fruits.push("Pear");
 // alert(fruits);
 
-let fruits = ["Apple", "Orange", "Pear"];
-// alert(fruits.shift());
+// let fruits = ["Apple", "Orange", "Pear"];
+// // alert(fruits.shift());
+// // alert(fruits);
+// // fruits.unshift("Apple");
+// // alert(fruits);
+// fruits.push("11","22");
+// fruits.unshift("333","444");
 // alert(fruits);
-// fruits.unshift("Apple");
-// alert(fruits);
-fruits.push("11","22");
-fruits.unshift("333","444");
-alert(fruits);
 
+// let fruits = ["Banana"];
+// let arr =fruits;
+// alert(arr ===fruits);
+// arr.push("Pear");
+// alert(fruits);
+// alert(arr);
+
+
+//循环
+// let arr = ["Apple", "Orange", "Pear"];
+// for(let i=0;i<arr.length;i++){
+//     alert(arr[i]);
+// }
+// for(let x of arr){
+//     alert(x);
+// }
+// for(let i in arr){
+//     alert(arr[i]);
+// }
+
+// // 关于 “length”
+// let fruits = [];
+// fruits[123] = "Apple";
+// alert( fruits.length ); // 124
+
+// let arr = [1, 2, 3, 4, 5];
+// arr.length = 2; // 截断到只剩 2 个元素
+// alert( arr ); // [1, 2]
+// arr.length = 5; // 又把 length 加回来
+// alert( arr[3] ); // undefined：被截断的那些数值并没有回来
+
+// // new Array()
+// // let arr = new Array("Apple","Pear","etc");
+// let arr =new Array(2);
+// alert(arr[0]);
+// alert(arr.length);
+
+//多维数组
+
+// //数组有自己的 toString 方法的实现，会返回以逗号隔开的元素列表。
+// let arr =[1,2,3];
+// alert(arr);
+// alert(String(arr)==='1,2,3');
+
+// alert([]+1);
+// alert([1]+1);
+// alert([1,2]+1);
+
+//不要使用 == 比较数组
+alert( [] == [] ); // false
+alert( [0] == [0] ); // false
+alert( 0 == [] ); // true
+alert('0' == [] ); // false
 
 
 
