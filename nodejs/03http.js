faa7762e63eba657f923345166daf416f82e473d
ruanshutian <1204970556@qq.com>
2022-09-04 20:13:07 +0800

    nodejs

--- nodejs/03http.js
+++ nodejs/03http.js
@@ -1,6 +1,24 @@
 
 
 const http = require('http');
-http.create
+let server = http.createServer();
+server.on('request',(req,res)=>{
+    console.log("someone visit our server");
 
+    // req 客户端相关
+    const  url = req.url;
+    const method = req.method;
+    const str = `我的中文 Your request url is ${url} and Your request method id ${method}`;
+    console.log(str);
+
+    // res 服务端相关
+
+    res.setHeader('Content-Type', 'text/html;charset=utf-8');//解决中文乱码
+    res.end(str)
+
+});
+
+server.listen(8000,()=>{
+    console.log("http server running at http://127.0.0.1:8000");
+})
 
