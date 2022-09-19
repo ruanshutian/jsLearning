466dd35cf613c411b26d645d2171e044b6f20d6e
ruanshutian <1204970556@qq.com>
2022-09-19 15:37:35 +0800

    class

---
+++ js_es6/尚硅谷es6/14_class_静态成员.js
@@ -0,0 +1,33 @@
+
+
+/**
+ * class
+ */
+
+// function Phone(brand,price){
+//     this.brand = brand ;
+//     this.price = price;
+// }
+//
+// Phone.prototype.call = function (){
+//     console.log(" i can call");
+// }
+//
+// let huawei = new Phone("huazi",5999);
+// huawei.call();
+// console.log(huawei);
+
+
+class Phone{
+    constructor(brand,price) {
+        this.brand = brand;
+        this.price =price;
+    }
+    // 
+     call(){
+         console.log("i can call you");
+     }
+}
+let oneplus = new Phone("1+",1999);
+console.log(oneplus);
+oneplus.call()
