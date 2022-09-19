6b0d6ffce82e4b1af295d54179a6fc2686cf3623
ruanshutian <1204970556@qq.com>
2022-09-19 13:53:26 +0800

    symbol

--- js_es6/尚硅谷es6/07_symbol使用.js
+++ js_es6/尚硅谷es6/07_symbol使用.js
@@ -4,3 +4,63 @@
  * Symbol 使用
  */
 
+ // 向对象中添加方法  up down
+
+// let game ={
+//     name:"rst",
+//     up :function (){
+//         console.log("up");
+//     },
+//     down:function (){
+//         console.log('down');
+//     }
+//
+// };
+//
+// let method = {
+//     up:Symbol(),
+//     down:Symbol()
+// }
+//
+// game[method.up] = function (){
+//     console.log("11");
+// }
+// game[method.down] = function (){
+//     console.log("22");
+// }
+//
+// console.log(game)
+
+//
+// let youxi = {
+//     name :"wzry",
+//     [Symbol("say")] :function (){
+//         console.log("say");
+//     },
+//     [Symbol("hello")]:function (){
+//     console.log("hello");
+//     }
+// };
+// console.log(youxi)
+
+/**
+ * Symbol 的内置值（Symbol属性）
+ */
+
+// class Person {
+//     static [Symbol.hasInstance](param){
+//         console.log(param);
+//         return false;
+//     }
+// }
+// let o ={};
+// console.log(o instanceof  Person);
+
+//
+const a1 = [1,2,3];
+const a2 = [4,5,6];
+
+a2[Symbol.isConcatSpreadable] = false;
+// a2[Symbol.isConcatSpreadable] = true;
+console.log(a1.concat(a2));
+
