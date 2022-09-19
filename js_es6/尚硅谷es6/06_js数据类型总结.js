3645c0f6881d7d3684e82463b9f669207fbd27ea
ruanshutian <1204970556@qq.com>
2022-09-19 11:06:55 +0800

    js数据类型

--- js_es6/尚硅谷es6/06_js数据类型总结.js
+++ js_es6/尚硅谷es6/06_js数据类型总结.js
@@ -1,26 +1,3 @@
-/**
- * es6   symbol 新的原始数据类型Symbol  表示独一无二的值
- */
-
-// 创建symbol
-
-let s =Symbol();
-let s2 =Symbol("sss");
-let s3 =Symbol('sss');
-
-console.log(s2,typeof s);
-
-console.log(s===s2);
-console.log(s2===s3);
-
-let s4 = Symbol.for('s4');
-let s5 = Symbol.for('s5');
-let s6 = Symbol.for('s4');
-console.log(s4 === s5)
-console.log(s4 === s6)
-
-// symbol不能运算
-
 
 /**
  *
