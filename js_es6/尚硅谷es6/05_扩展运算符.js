d18aeafd131b853c2a550e05b3f9ac9e5943342c
ruanshutian <1204970556@qq.com>
2022-09-19 10:43:06 +0800

    扩展运算符

--- js_es6/尚硅谷es6/05_扩展运算符.js
+++ js_es6/尚硅谷es6/05_扩展运算符.js
@@ -17,9 +17,21 @@
 
 // 数组的合并
 
-const arr1 = [1,2];
-const arr2 = [3,4];
-const arr3 = [...arr1, ...arr2];
-console.log(arr3)
+// const arr1 = [1,2];
+// const arr2 = [3,4];
+// const arr3 = [...arr1, ...arr2];
+// console.log(arr3)
 
+// 数组的克隆
 
+//  const a1 = ['a','b','c'];
+//  const a2 = ['d','e','f'];
+//  const a3 = [...a1];
+// console.log(a3);
+
+//将伪数组转换为真正的数组
+
+const a = document.querySelectorAll('div');
+console.log(a);
+const arr = [...a];
+console.log(arr);
