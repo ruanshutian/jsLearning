04c16a48b30047e3e0446c35adff0c4772702735
ruanshutian <1204970556@qq.com>
2022-09-20 09:33:13 +0800

    class

--- js_es6/尚硅谷es6/16_class类继承.js
+++ js_es6/尚硅谷es6/16_class类继承.js
@@ -5,29 +5,44 @@
  */
 
 class Phone{
-    constructor(brand, price) {
-        this.brand = brand;
-        this.price = price;
+    // constructor(brand, price) {
+    //     this.brand = brand;
+    //     this.price = price;
+    // }
+    get price(){
+        console.log("get price");
+        return'i love you';
+    }
+    set price(newVal){
+        console.log('set value');
     }
     call() {
         console.log("i can call");
     }
 }
 
-class SmartPhone extends Phone{
-    constructor(brand , price , color ,size) {
-        super(brand,price);
-        this.color = color;
-        this.size = size;
-    }
-    photo(){
-        console.log("拍照");
-    }
-    playGame(){
-        console.log("玩游戏");
-    }
-}
+// class SmartPhone extends Phone{
+//     constructor(brand , price , color ,size) {
+//         super(brand,price);
+//         this.color = color;
+//         this.size = size;
+//     }
+//     photo(){
+//         console.log("拍照");
+//     }
+//     playGame(){
+//         console.log("玩游戏");
+//     }
+//     //重写
+//     call(){
+//         console.log("i can fly")
+//     }
+// }
+//
+// const  test = new SmartPhone("xiaomi",799,"black",'ss');
+// console.log(test);
+// test.call()
 
-const  test = new SmartPhone("xiaomi",799,"black",'ss');
-console.log(test);
-
+let p = new Phone();
+console.log(p.price)
+p.price = 'free';
