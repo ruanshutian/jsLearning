46b373c363e4dd9030372f031364768a2ce28e22
ruanshutian <1204970556@qq.com>
2022-10-10 09:24:39 +0800

    动态组件

--- vue/vue3/viteCode/src/components/018动态组件/App.vue
+++ vue/vue3/viteCode/src/components/018动态组件/App.vue
@@ -1,8 +1,21 @@
 <template>
+  <hr/>
+
+  <hr/>
   <div>
     <button class="home" @click="myComponent='MyHome'">my home</button>
     <button class="movie" @click="myComponent='MyMovie'">my movie</button>
     <component :is="myComponent"></component>
+  </div>
+  <hr/>
+  <div> keep-alive </div>
+  <hr/>
+  <div>
+    <button class="home" @click="myComponent='MyHome'">my home</button>
+    <button class="movie" @click="myComponent='MyMovie'">my movie</button>
+    <keep-alive>
+      <component :is="myComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
--- vue/vue3/viteCode/src/components/018动态组件/MyHome.vue
+++ vue/vue3/viteCode/src/components/018动态组件/MyHome.vue
@@ -1,10 +1,30 @@
 <template>
-<div>this is my Home</div>
+<div>this is my Home </div>
+  <span>
+    count = {{count}}
+  </span>
+  <button @click="addCount">addCount</button>
 </template>
 
 <script>
 export default {
-  name: "MyHome"
+  name: "MyHome",
+  data(){
+    return{
+      count:0
+    }
+  },
+  methods:{
+    addCount(){
+      this.count++;
+    }
+  },
+  created() {
+    console.log("created")
+  },
+  unmounted() {
+    console.log('unmounted');
+  }
 }
 </script>
 
