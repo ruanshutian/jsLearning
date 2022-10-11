bb4776d92a63e2bf35a4be1980e19c20df103230
ruanshutian <1204970556@qq.com>
2022-10-11 18:24:59 +0800

    basis.

--- TypeScript/002basis.js
+++ TypeScript/002basis.js
@@ -1,4 +1,24 @@
+/**
+ * 指定了类型为number，所以赋值为字符串的时候报错
+ */
 var a;
 a = 10;
 a = 20;
-// a = "hello";//指定了类型为number，所以赋值为字符串的时候报错
+// a = "hello"; //指定了类型为number，所以赋值为字符串的时候报错
+var b;
+b = "hello";
+/**
+ * 声明变量直接进行赋值
+ */
+// let c:boolean = false;
+// c = true;
+// c = 123; //
+/**
+ * 如果变量的声明和赋值是同时进行的，TS可以自动变量进行类型检测
+ */
+var c = false;
+c = true;
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(3, 4));
--- TypeScript/002basis.ts
+++ TypeScript/002basis.ts
@@ -1,12 +1,33 @@
 
-let a : number
 
+/**
+ * 指定了类型为number，所以赋值为字符串的时候报错
+ */
+
+let a : number
 a = 10;
 a = 20;
-// a = "hello";//指定了类型为number，所以赋值为字符串的时候报错
+// a = "hello"; //指定了类型为number，所以赋值为字符串的时候报错
+
+let b:string;
+b= "hello";
+/**
+ * 声明变量直接进行赋值
+ */
+// let c:boolean = false;
+// c = true;
+// c = 123; //
+
+/**
+ * 如果变量的声明和赋值是同时进行的，TS可以自动变量进行类型检测
+ */
+let c = false;
+
+ c = true;
 
 
+function sum(a:number , b:number ){
+    return a+b;
+}
 
-
-
-
+console.log(sum(3,4));
